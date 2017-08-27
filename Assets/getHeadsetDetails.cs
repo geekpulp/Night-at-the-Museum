b1cdbf5d3d0b6f47bf5584f2756758b6326177e2
Glen Young <glen@geekpulp.co.nz>
2017-08-27 15:50:45 +1200

    Save

---
+++ Assets/getHeadsetDetails.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getHeadsetDetails : MonoBehaviour {
+
+    //var
+    public void Test() {
+        Debug.Log("testing my method is being called");
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
