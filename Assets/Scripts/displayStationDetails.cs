6947c51b29f0dd648204f943bb5d367332a655f9
Glen Young <glen@geekpulp.co.nz>
2017-08-31 22:18:18 +1200

    Can click on things again and have sorted the frame rate to over 60fps

--- Assets/Scripts/displayStationDetails.cs
+++ Assets/Scripts/displayStationDetails.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class getHeadsetDetails : MonoBehaviour {
+public class displayStationDetails : MonoBehaviour {
 
     //var
-    public void Test() {
-        Debug.Log("testing my method is being called");
+	public GameObject boom;
+
+    public void Headset() {
+			boom.SetActive(!boom.activeInHierarchy);
+			Debug.Log("testing my method is being called");
     }
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
