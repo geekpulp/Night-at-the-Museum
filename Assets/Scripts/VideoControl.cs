744f362e1d1a87c37b141ffb95499824b72f776e
Glen Young <glen@geekpulp.co.nz>
2017-11-27 15:02:34 +1300

    adding video controls

---
+++ Assets/Scripts/VideoControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoControl : MonoBehaviour {
+
+    private UnityEngine.Video.VideoPlayer videoPlayer;
+    public GameObject videoOrbToPlay;
+
+    [SerializeField]
+    //private AudioSource audioSource;
+
+    void Start () {
+        videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer> ();
+
+
+        //if (videoPlayer.clip != null) 
+        //{
+        //    videoPlayer.EnableAudioTrack (0, true);
+        //    videoPlayer.SetTargetAudioSource(0, audioSource);
+        //}
+    }
+
+    //Check if input keys have been pressed and call methods accordingly.
+    void Update(){
+        //Play or pause the video.
+        if (Input.GetKeyDown ("space")) 
+        {
+            if (videoPlayer.isPlaying)
+                videoPlayer.Pause ();
+            else
+                videoPlayer.Play();
+                //audioSource.Play();
+        }
+
+    }
+
+    public void playpause() {
+        if (videoPlayer.isPlaying)
+            videoPlayer.Pause();
+        else
+            videoPlayer.Play();
+    }
+
+    public void showplayer(GameObject videoOrbToPlay)
+    {
+        videoOrbToPlay.SetActive;
+    }
+
+}
